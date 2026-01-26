04a0e8f462864ea65c017685b9336dd5b047cebd
Maxim Chellak <maxim.chellak@kbis.co.il>
2026-01-26 17:02:11 +0200

    .

---
+++ StarNet.JWTManager/TokenInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarNet.JWTManager
+{
+    public class TokenInfo
+    {
+        public string Address { get; set; }
+        public int UserId { get; set; }
+        public string TokenIssuerName { get; set; }
+        public string SecurityKey { get; set; }
+    }
+}
